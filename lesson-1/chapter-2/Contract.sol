52c241c4c89808f578022b457be467e4b75ec108
Steve Boyer <stephen.a.boyer@gmail.com>
2021-11-10 20:03:26 -0600

    function createZombie

--- lesson-1/chapter-2/Contract.sol
+++ lesson-1/chapter-2/Contract.sol
@@ -2,15 +2,23 @@
 pragma solidity ^0.4.25;
 
       contract ZombieFactory {
-          // this will be permanently stored in the blockchain
+          // This variable will be permanently stored in the blockchain
+          // This is a State variable
           uint dnaDigits = 16;
           uint dnaModulus = 10 ** dnaDigits;
 
+          // This Struct defines our type and the types variables
           struct Zombie {
               string name;
               uint dna;
           }
 
           Zombie[] public zombies;
+          // _name variable is a reference type
+          // _dna variable is a value type
+          function createZombie (string memory _name, uint _dna) public {
+            // Here we create a new Zombie and push it to out zombie array
+            zombies.push(Zombie(_name, _dna));  
+          }
 
       }
