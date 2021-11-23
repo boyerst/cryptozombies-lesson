4423e1e33c05136af5dd836d370e2696f4989d2f
Steve Boyer <stephen.a.boyer@gmail.com>
2021-11-23 11:37:15 -0600

    function feedAndMultiply creates zombified Dna for zombie victim

--- lesson-2/chapter-6/zombiefeeding.sol
+++ lesson-2/chapter-6/zombiefeeding.sol
@@ -6,9 +6,26 @@ contract ZombieFeeding is ZombieFactory {
 
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-    // We require in order to verify that msg.sender is equal to this zombie's owner
+    // We 'require' in order to verify that msg.sender is equal to this zombie's owner
+      // Bc we don't want to let someone else feed our zombie
       // So we match the index of _zombieId in the zombieToOwner mapping to msg.sender
+      // We verify that msg.sender is equal to the zombie's owner
+      // We do this by matching the address that owns the zombie which we find via _zombieId index in zombieToOwner mapping
     require(msg.sender == zombieToOwner[_zombieId]);
+    // We need to get the zombie's DNA
+    // First we create a local zombie (myZombie), whose data is stored permanentely in 'storage'
+    // We set the variable myZombie to be equal to the index of _zombieId in our zombies array
+      // ❓Where is zombies array?
     Zombie storage myZombie = zombies[_zombieId];
+    // Ensure _targetDna isn't longer than 16 digits
+    _targetDna == _targetDna % dnaModulus;
+    // NewDna is the average between the feeding zombie's DNA and the target's DNA
+    // We can access the properties of myZombie with myZombie.<property> (Variable.property)
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    // We call createZombie, and calling the required parameters... 
+      // " (string memory _name, uint _dna) "
+    _createZombie("NoName", newDna);
+    // 
+
   }
 }
