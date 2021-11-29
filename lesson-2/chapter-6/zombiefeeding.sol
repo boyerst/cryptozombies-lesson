62f3e8820f1fe2d159e873cdf511082c1e400243
Steve Boyer <stephen.a.boyer@gmail.com>
2021-11-29 14:15:08 -0600

    zombie Dna made from kitties reflected

--- lesson-2/chapter-6/zombiefeeding.sol
+++ lesson-2/chapter-6/zombiefeeding.sol
@@ -8,6 +8,7 @@ import "./zombiefactory.sol";
 // Here we create an interface for the the function getKitty() from CryptoKitty smart contract
   // Now we can grab data from said function, in this case we want 'genes'
 contract KittyInterface {
+  // We can just pass in a kittyId to retrieve the values from the CryptoKitty smart contract
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
@@ -50,27 +51,42 @@ contract ZombieFeeding is ZombieFactory {
     // NewDna is the average between the feeding zombie's DNA and the target's DNA
     // We can access the properties of myZombie with myZombie.<property> (Variable.property)
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Here we add an if statement comparing the keccak hashes of _species (the 3rd argument, string, we added above) to that of the string 'kitty'
+    // We want to replace the last two digits of newDna with 99 as this denotes cat Dna
+    // We modulus 100 of the new Dna to get return a whole number remainder
+      // Then we add 99 to change the last two digits to the desired numbers
+      // ❓Where are _species and "kitty" declared? In CryptoKitty contract?
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }    
     // We call createZombie, and calling the required parameters... 
       // " (string memory _name, uint _dna) "
       // We ran into an issue here....we tried calling the _createZombie function from within ZombieFeeding, but _createZombie is a private function inside ZombieFactory. This means none of the contracts that inherit from ZombieFactory can access it
     _createZombie("NoName", newDna);
   }
 
+
   // This function will make it so that zombies made from kitties have some unique cat like features
   // Here we make a function that takes two parameters
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     // We declare a uint named kittyDna
+    // We are going to assign a value to this uint be retreiving it from the KittyInterface
     uint kittyDna;
-    // Then we call (via the interface) the kittyContract.getKitty function with _kittyId and store genes in the kittyDna we just declared above
+    // Then we call (via the interface) the kittyContract.getKitty function with _kittyId (because it takes uint256 _id) and store genes in the kittyDna we just declared above
     // We only care about the last value (genes), so we leave all other return fields blank but still insert commas
+    // ↓↓This is the val we want  ↓↓Call interface w the contract which takes an Id
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     // Then we call feedAndMultiply and pass it both _zombieId and kittyDna
-    feedAndMultiply(_zombieId, kittyDna);
+      // We added the parameter 'kitty' because the if statement in feedAndMultiply takes it❓
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   
 
   }
 
 
-
-
 }
+
+
+
+
+
